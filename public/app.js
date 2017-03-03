62f78ba4f97cfbff057f58fa0e3be234fc956f77
nothingtwoit <lotu@tuta.io>
2017-03-02 18:38:07 -0800

    .,

--- public/app.js
+++ public/app.js
@@ -1,20 +1,26 @@
-function onSubmit() {
-  document.getElementById("submitme").addEventListener("click", function(event) {
-    event.preventDefault()
-    let unCheckedPostalCode = document.getElementById('myform').zipcode.value;
-    let postalCode = parseInt(unCheckedPostalCode);
-    console.log(postalCode);
-      let content = {
-          postalCode,
-      }
-      if (Number.isInteger(postalCode)) {
-
-          $.post('/data', {info: content}, function (data) {
-              console.log("working");
-          })
-      } else {
-        alert(NaN);
-      }
-  });
-}
-onSubmit();
+  //
+  // document.getElementById("submitme").addEventListener("click", function(event) {
+  //   event.preventDefault();
+  //   let unCheckedPostalCode = document.getElementById('myform').zipcode.value;
+  //   let postalCode = parseInt(unCheckedPostalCode);
+  //   console.log(postalCode);
+  //     let content = {
+  //         postalCode,
+  //     }
+  //     if (Number.isInteger(postalCode)) {
+  //
+  //         $.post('/data', {info: content}, function (data) {
+  //             console.log("working");
+  //         })
+  //     } else {
+  //       alert(NaN);
+  //     }
+  // });
+  
+  
+  function onSubmit() {
+    console.log("working");
+    var uncheckedPostalCode = document.getElementById("zipcode").value;
+    console.log(uncheckedPostalCode);
+      
+  }
