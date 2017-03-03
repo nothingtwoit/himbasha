046e529d9584f89a8605e3bcfd57648fe8fc6dc8
nothingtwoit <lotu@tuta.io>
2017-03-02 20:31:39 -0800

    data

--- public/app.js
+++ public/app.js
@@ -1,13 +1,16 @@
  document.getElementById("submitme").addEventListener("click", function(event) {
         event.preventDefault()
-        let unCheckedPostalCode = document.getElementById('myform').zipcode.value;
+        var unCheckedPostalCode = document.getElementById('myform').zipcode.value;
         var postalCode = parseInt(unCheckedPostalCode);
 
         if (Number.isInteger(postalCode) && postalCode.toString().length == 5) {
             $.post('/data', {info: postalCode}, function (data) {
+                console.log(data);
             })
+             window.location.href= '/com/stores.html';
+            // window.location.replace('http://google.com');
         } else {
-            alert(NaN);
+            alert("please enter a five digit zip code");
         }
     });
 
